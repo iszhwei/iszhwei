39d9b352101e6419f90be2c5f3061626c9f7121b
iszhwei <iszhwei@163.com>
2024-05-01 16:34:11 +0800

    push blog

--- dist/assets/valgrind.html-gp7Krogn.js
+++
@@ -1,7 +0,0 @@
-import{_ as s}from"./plugin-vue_export-helper-x3n3nnut.js";import{o as n,c as a,a as l}from"./app-9OmaxHRC.js";const e={},i=l(`<p>valgrind</p><div class="language-bash line-numbers-mode" data-ext="sh"><pre class="shiki one-dark-pro" style="background-color:#282c34;" tabindex="0"><code><span class="line"><span style="color:#61AFEF;">&quot; 安装方式：</span></span>
-<span class="line"><span style="color:#61AFEF;">$ sudo apt install valgrind</span></span>
-<span class="line"><span style="color:#61AFEF;">    </span></span>
-<span class="line"><span style="color:#61AFEF;">&quot;</span><span style="color:#ABB2BF;"> </span><span style="color:#98C379;">使用方式:</span></span>
-<span class="line"><span style="color:#61AFEF;">&quot; 编译代码时，需要加上-g</span></span>
-<span class="line"><span style="color:#61AFEF;">$ valgrind --tool=memcheck --leak-check=full ./test</span></span>
-<span class="line"></span></code></pre><div class="line-numbers" aria-hidden="true"><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div></div></div>`,2),c=[i];function o(r,t){return n(),a("div",null,c)}const v=s(e,[["render",o],["__file","valgrind.html.vue"]]);export{v as default};
